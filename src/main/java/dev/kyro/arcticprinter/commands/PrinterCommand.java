e8dcdd8c8af016a853a42ff1e54f125c7b47cb35
KyroKrypt <kyrokrypt@gmail.com>
2021-02-11 10:11:30 -0800

    Modified PrinterCommand Added check for items in inventory and armor on player

--- src/main/java/dev/kyro/arcticprinter/commands/PrinterCommand.java
+++ src/main/java/dev/kyro/arcticprinter/commands/PrinterCommand.java
@@ -5,11 +5,13 @@ import dev.kyro.arcticapi.misc.AOutput;
 import dev.kyro.arcticprinter.controllers.PrinterManager;
 import dev.kyro.arcticprinter.enums.PrinterEndReason;
 import dev.kyro.arcticprinter.objects.PrinterPlayer;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.inventory.ItemStack;
 
 public class PrinterCommand implements CommandExecutor {
 
@@ -24,16 +26,34 @@ public class PrinterCommand implements CommandExecutor {
 
         Player player = (Player) sender;
 
-        if(!FPlayers.getInstance().getByPlayer(player).isInOwnTerritory()) {
-
-            AOutput.error(player, "Not in your own territory");
-            return false;
-        }
-
         if(PrinterManager.inPrinter(player)) {
 
             PrinterManager.getPrinterPlayer(player).exitPrinter(PrinterEndReason.COMMAND_DISABLE);
         } else {
+
+            if(!FPlayers.getInstance().getByPlayer(player).isInOwnTerritory()) {
+
+                AOutput.error(player, "Not in your own territory");
+                return false;
+            }
+
+            for(ItemStack itemStack : player.getInventory()) {
+
+                if(itemStack != null && itemStack.getType() != Material.AIR) {
+
+                    AOutput.error(player, "Inventory is not empty");
+                    return false;
+                }
+            }
+
+            for(ItemStack itemStack : player.getInventory().getArmorContents()) {
+
+                if(itemStack != null && itemStack.getType() != Material.AIR) {
+
+                    AOutput.error(player, "Please take off your armor");
+                    return false;
+                }
+            }
 
             new PrinterPlayer(player);
         }
